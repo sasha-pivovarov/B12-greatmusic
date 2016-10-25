ab98bec2d34d8e57cf2f8628790a6481b3c3cf31
sasha-pivovarov <cantwork.pivovarov@gmail.com>
2016-10-25 06:31:38 -0400

    Add files via upload

---
+++ greatmusicthread_grabber.py
@@ -0,0 +1,20 @@
+import requests
+import re
+import json
+
+#Modify this to grab whatever. The regex can probably be improved too, but I didn't care enough.
+page_urls = ['http://www.bay12forums.com/smf/index.php?topic=82547.' + str(x) for x in range(0,4306, 15)]
+
+def grab_youtube_links(url):
+    source = str(requests.get(url).content)
+    return re.findall('a href="(http://www.youtu.*?)"', source)
+
+link_list = []
+
+for url in page_urls:
+    link_list.extend(grab_youtube_links(url))
+    
+print(len(link_list))
+
+with open('yt_links_from_b12.json', 'w') as file:
+    json.dump(link_list, file)
---
+++ greatmusicthread_poster.py
@@ -0,0 +1,98 @@
+import httplib2
+import os
+import sys
+import json
+import re
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.file import Storage
+from oauth2client.tools import argparser, run_flow
+
+
+# The CLIENT_SECRETS_FILE variable specifies the name of a file that contains
+# the OAuth 2.0 information for this application, including its client_id and
+# client_secret. You can acquire an OAuth 2.0 client ID and client secret from
+# the Google Cloud Console at
+# https://cloud.google.com/console.
+# Please ensure that you have enabled the YouTube Data API for your project.
+# For more information about using OAuth2 to access the YouTube Data API, see:
+#   https://developers.google.com/youtube/v3/guides/authentication
+# For more information about the client_secrets.json file format, see:
+#   https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+
+CLIENT_SECRETS_FILE = "client_secrets.json"
+
+# This variable defines a message to display if the CLIENT_SECRETS_FILE is
+# missing.
+MISSING_CLIENT_SECRETS_MESSAGE = """
+WARNING: Please configure OAuth 2.0
+
+To make this sample run you will need to populate the client_secrets.json file
+found at:
+
+%s
+
+with information from the Cloud Console
+https://cloud.google.com/console
+
+For more information about the client_secrets.json file format, please visit:
+https://developers.google.com/api-client-library/python/guide/aaa_client_secrets
+""" % os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                   CLIENT_SECRETS_FILE))
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account.
+YOUTUBE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+
+def get_authenticated_service():
+    flow = flow_from_clientsecrets(CLIENT_SECRETS_FILE, scope=YOUTUBE_SCOPE,
+                                   message=MISSING_CLIENT_SECRETS_MESSAGE)
+
+    storage = Storage("%s-oauth2.json" % sys.argv[0])
+    credentials = storage.get()
+
+    if credentials is None or credentials.invalid:
+        credentials = run_flow(flow, storage)
+
+    return build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+                 http=credentials.authorize(httplib2.Http()))
+
+
+def add_video_to_playlist(youtube,videoID,playlistID):
+    add_video_request=youtube.playlistItems().insert(
+        part="snippet",
+        body={
+            'snippet': {
+                'playlistId': playlistID, 
+                'resourceId': {
+                    'kind': 'youtube#video',
+                    'videoId': videoID
+                }
+                #'position': 0
+            }
+        }
+        ).execute()
+
+playlist_id = input('Please input the Youtube ID of the playlist you wish to upload to.')
+if __name__ == '__main__':
+    youtube = get_authenticated_service()
+    with open('yt_links_from_b12.json', 'r') as io:
+        yt_links = json.load(io)
+    added = 0
+    failed = 0
+    for link in yt_links:
+        video_id = re.findall('v=(.*?)(?:&|$)', link)
+        try:
+            add_video_to_playlist(youtube, video_id, playlist_id)
+            print('Added video with id %s'%video_id)
+            added += 1
+        except HttpError:
+            print('Could not find video with id %s'%video_id)
+            failed += 1
+    
+    print('DONE. Added %d videos, failed to add %d videos.'%(added, failed))
